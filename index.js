70ca10440d6f95ac955081bc6b23c8f0a9318e9c
xXKARCXx <aw3528231@gmail.com>
2025-03-18 18:34:09 +0000

    Revert "style update for frontpage"
    This reverts commit af8f7ffad0a0998761a6073c25914ebc694f0956.

--- index.js
+++ index.js
@@ -4,21 +4,3 @@
 // This will run the code.
 console.log("entrypoint");
 const app = require("./app/app.js");
-
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-// Serve static files from the 'static' folder
-app.use(express.static("static"));
-
-// Set frontpage.html as the default page
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "static", "frontpage.html"));
-});
-
-// Start server on port 3000
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-  });
